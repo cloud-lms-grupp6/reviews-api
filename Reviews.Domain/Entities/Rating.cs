75f8b0c62c6323af9d31db45861ada54e448e9de
Duki <dukilicious@hotmail.com>
2026-05-27 23:41:34 +0200

    add Review entity and Rating value object

---
+++ Reviews.Domain/Entities/Rating.cs
@@ -0,0 +1,22 @@
+namespace Reviews.Domain.Entities;
+
+
+public sealed class Rating
+{
+    public int Value { get; }
+
+    private Rating(int value)
+    {
+        Value = value;
+    }
+
+    public static Rating Create(int value)
+    {
+        if (value < 1 || value > 5)
+        {
+            throw new ArgumentException("Rating must be between 1 and 5.");
+        }
+
+        return new Rating(value);
+    }
+}
