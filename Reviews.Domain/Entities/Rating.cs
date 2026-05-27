6b2ec0303d852349e88d4cdf106332858e3fe023
Duki <dukilicious@hotmail.com>
2026-05-28 00:06:21 +0200

    refactor(KAN-44): encapsulate Rating creation within Review

--- Reviews.Domain/Entities/Rating.cs
+++ Reviews.Domain/Entities/Rating.cs
@@ -14,7 +14,7 @@ public sealed class Rating
     {
         if (value < 1 || value > 5)
         {
-            throw new ArgumentException("Rating must be between 1 and 5.");
+            throw new ArgumentException("Rating must be between 1 and 5 stars.");
         }
 
         return new Rating(value);
